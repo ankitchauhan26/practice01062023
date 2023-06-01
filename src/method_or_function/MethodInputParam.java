e0e3dc28215cc82aefb28a08509b6398ffaadee8
ankitchauhan26 <68079328+ankitchauhan26@users.noreply.github.com>
2023-06-01 11:13:38 +0530

    Square

--- src/method_or_function/MethodInputParam.java
+++ src/method_or_function/MethodInputParam.java
@@ -8,11 +8,19 @@ public class MethodInputParam {
         return msg;
     }
 
+    public int makeSquare(int num){
+        int square = num*num;
+        return square;
+    }
+
     public static void main(String[] args) {
          MethodInputParam obj =new MethodInputParam();
         String result= obj.welcome("Ankit"); // we need to pass the value
         System.out.println(result);
         String result1= obj.welcome("Rahul");
         System.out.println(result1);
+
+       int square  = obj.makeSquare(3);
+        System.out.println(square);
     }
 }
