d113f5bc8dc8247beb8a3433a376cbeea03466ad
ankitchauhan26 <68079328+ankitchauhan26@users.noreply.github.com>
2023-06-01 12:28:12 +0530

    Calculation

--- src/method_or_function/CalculatorInputParam.java
+++ src/method_or_function/CalculatorInputParam.java
@@ -13,12 +13,29 @@ public class CalculatorInputParam {
         return sub;
     }
 
+    public int multiplication(int a, int b){
+
+        int mul = a*b;
+        return mul;
+    }
+    public int division(int a, int b){
+
+        int div = a/b;
+        return div;
+    }
+
     public static void main(String[] args) {
         CalculatorInputParam obj = new CalculatorInputParam();
         int sum = obj.addition(20,10);
         System.out.println("Addition of a and b is : =" + sum);
         sum = obj.addition(230,230);
         System.out.println("Addition of a and b is : =" + sum);
+        int result = obj.division(20,10);
+        System.out.println("Division of a and b is : =" + result);
+        result = obj.Subtraction(30,20);
+        System.out.println("Subtraction of a and b is : =" + result);
+        result = obj.multiplication(3,2);
+        System.out.println("Multiplication of a and b is : =" + result);
 
     }
 }
