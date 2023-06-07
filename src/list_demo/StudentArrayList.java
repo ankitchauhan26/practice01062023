070360172f4d1e62f1d53b7ea62c12ae92767d5f
ankitchauhan26 <68079328+ankitchauhan26@users.noreply.github.com>
2023-06-07 16:25:40 +0530

    Collection Day2

---
+++ src/list_demo/StudentArrayList.java
@@ -0,0 +1,41 @@
+package list_demo;
+
+import model.Student;
+
+import java.util.ArrayList;
+
+public class StudentArrayList {
+
+    // create a method which will create Student Array List
+
+    public ArrayList<Student> createStudentList(){
+        ArrayList <Student> studentArrayList =new ArrayList<>();
+
+        // now we need to add information about students
+
+        Student student1 = new Student(1,"Ankit","CS");
+        Student student2 = new Student(2,"Mukesh","IT");
+        Student student3 = new Student(3,"Rahul","EC");
+
+        studentArrayList.add(student1);
+        studentArrayList.add((student2));
+        studentArrayList.add(student3);
+
+        return studentArrayList;
+    }
+
+
+    public static void main(String[] args) {
+
+        StudentArrayList obj = new StudentArrayList();
+        ArrayList<Student> studentArrayList = obj.createStudentList();
+
+        for(int i = 0;i<studentArrayList.size();i++){
+            System.out.println("Printing "+ studentArrayList.get(i).getName()+ " Roll number"
+                    + studentArrayList.get(i).getRollNo()+ " Branch "+ studentArrayList.get(i).getBranch() );
+        }
+
+
+
+    }
+}
--- src/model/Student.java
+++ src/model/Student.java
@@ -18,4 +18,27 @@ public class Student {
     // getter and setter method
 
 
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
 }
